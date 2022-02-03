430358da271fb64b529c67c0220c13490a55f800
Michał Hyla <michal@Mac-mini-Micha-4.local>
2022-02-03 02:30:13 +0100

    added timer and distance

--- SimpleRacingGame.py
+++ SimpleRacingGame.py
@@ -10,7 +10,7 @@ FPS = 60
 
 pygame.init()
 pygame.font.init()
-myfont = pygame.font.SysFont('Comic Sans MS', 30)
+myfont = pygame.font.SysFont('Lucida Console', 30)
 
 
 clock = pygame.time.Clock()
@@ -24,18 +24,28 @@ pygame.display.set_caption("1/4 mile race")
 
 def main():
     speed = 0
+    distance = 0
+    time = 0
     running = True
     
     while running:
         keys = pygame.key.get_pressed()
-        
+        speed_surface = myfont.render(str(round(speed*3.6)) + " km/h", False, (0, 0, 0))
+        dist_surface = myfont.render(str(round(distance)) + " m", False, (0, 0, 0))      #deklaracja powierzchni dystansu
+        timer_surface = myfont.render(str(round(time,2)) + " s", False, (0, 0, 0))
+
         WIN.blit(background, bgrect)
         WIN.blit(background, bgrect.move(bgrect.width, 0))
-        WIN.blit(vehicle,(40, HEIGHT-140))
-        textsurface = myfont.render(str(round(speed*3.6)), False, (0, 0, 0))
-        WIN.blit(textsurface,(0,0))
-        bgrect.move_ip(METER*speed/FPS*(-1), 0)
-        print (bgrect.right)
+        WIN.blit(vehicle,(40, HEIGHT-140))                  #render autka i tła
+        bgrect.move_ip(METER*speed/FPS*(-1), 0)             #przesuwanie tła
+
+        WIN.blit(speed_surface,(0,0))                       #render napisów
+        WIN.blit(dist_surface,(0,30))
+        WIN.blit(timer_surface,(0,60))
+        
+        distance = distance+speed/FPS
+        time = time+1/FPS
+
         if bgrect.right <= 0 and bgrect.left <= WIDTH:
             bgrect.x = 0
         for event in pygame.event.get():
@@ -44,20 +54,22 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_q:
                     running = False
+
         if keys[pygame.K_UP]:
             speed=speed+2.24/FPS
             if speed > 53.3:
                 speed=53.3
+
         else:
             speed=speed-2.24/FPS
             if speed < 0:
                 speed = 0
+
         if keys[pygame.K_DOWN]:
             speed=speed-18/FPS
             if speed < 0:
                 speed = 0
-
-        print(round(speed*3.6,2))
+                
         clock.tick(FPS)
         pygame.display.flip()
     pygame.quit()
